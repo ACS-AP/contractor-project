7eca08bd273f46f25d3823c8ca017809953376ba
ArnavPag <arnav.pagidyala@students.dominican.edu>
2021-11-09 20:42:26 -0800

    Added templates

--- app.py
+++ app.py
@@ -1,21 +1,64 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, make_response
 from pymongo import MongoClient
 from datetime import datetime
-import os
+from werkzeug.utils import redirect
 
+import bcrypt
+import os
 
 app = Flask(__name__)
 
-host = os.environ.get('MONGODB_URI')
-client = MongoClient(host=host)
-db = client.get_database('contractor-project')
-
-users = db.users
-charities = db.charities
-
 @app.route('/')
-def index():
-    return render_template('index.html')
+def charity_tracker_index():
+  return render_template('new_donation.html') 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route('/login')
+def login_form():
+  return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+  user = {
+    'email': request.form.get('email'),
+    'password': request.form.get('password')
+  }
+  print(user)
+  return render_template('new_donation.html')
+
+@app.route('/signup')
+def signup_form():
+  return render_template('signup.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+  new_user = {
+    'email': request.form.get('email'),
+    'password': request.form.get('password'),
+    'confirm_password': request.form.get('confirm_password'),
+  }
+  print(new_user)
+  return render_template('login.html')
+
+@app.route('/profile')
+def donor_profile():
+  return render_template('profile.html')
+
+@app.route('/charity')
+def charity_profile():
+  return render_template('charity.html')
+
+@app.route('/donation/new')
+def track_donation():
+  return render_template('new_donation.html') 
+
+@app.route('/donation', methods=['POST'])
+def donation_submit():
+  donation = {
+    'charity_name': request.form.get('charity_name'),
+    'amount_in_cents': request.form.get('donation_amount'),
+    'date': request.form.get('date_donated'),
+  }
+  print(donation)
+  return render_template('new_donation.html')
+
