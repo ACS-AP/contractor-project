69fcfeab59ac3443df543278a4aba5751faba51b
ArnavPag <arnav.pagidyala@students.dominican.edu>
2021-11-11 17:10:55 -0800

    added partials

--- app.py
+++ app.py
@@ -62,3 +62,5 @@ def donation_submit():
   print(donation)
   return render_template('new_donation.html')
 
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 8000))
